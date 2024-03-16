d9a93514ea37244ef2843bf2c6891e8e2c601987
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-03-16 18:09:36 +0100

    Add function

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify  # TODO , request
 
-# TODO from http import HTTPStatus
+from http import HTTPStatus
 
 
 app = Flask(__name__)
@@ -22,6 +22,14 @@ recipes = [
 @app.route("/recipes", methods=["GET"])
 def get_recipes():
     return jsonify({"data": recipes})
+
+
+@app.route("/recipes/<int:recipe_id>", methods=["GET"])
+def get_recipe(recipe_id: int):
+    recipe = next((recipe for recipe in recipes if recipe["id"] == recipe_id), None)
+    if recipe:
+        return jsonify({"data": recipe})
+    return jsonify({"message": "recipe not found"}), HTTPStatus.NOT_FOUND
 
 
 if __name__ == "__main__":
--- tests/e2e/test_app.py
+++ tests/e2e/test_app.py
@@ -6,3 +6,11 @@ class TestEndpoints(unittest.TestCase):
     def test_recipes_returns_expected_result(self):
         response = requests.get("http://localhost:5000/recipes")
         self.assertEqual(2, len(response.json()["data"]))
+
+    def test_recipes_search_id_returns_expected_result_if_exist(self):
+        response = requests.get("http://localhost:5000/recipes/2")
+        self.assertEqual(2, response.json()["data"]["id"])
+
+    def test_recipes_search_id_returns_expected_result_if_does_not_exis(self):
+        response = requests.get("http://localhost:5000/recipes/0")
+        self.assertEqual("recipe not found", response.json()["message"])
